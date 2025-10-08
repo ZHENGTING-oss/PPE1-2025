af6924068ff7fac1c10a19effc4b158a6c6b1cd0
ZHENG TING <45017931@parisnanterre.fr>
2025-10-08 20:46:24 +0200

    tp1-ex1

---
+++ Exercices/comptes.sh
@@ -0,0 +1,8 @@
+#!usr/bin/bash
+CHEMIN=~/Desktop/Plurital/PPE1/PPE1-2025/Exercice1/ann
+echo "Nombre de lieux en 2016:"
+cat $CHEMIN/2016/* | grep Location | wc -l
+echo "Nombre de lieux en 2017:"
+cat $CHEMIN/2017/* | grep Location | wc -l
+echo "Nombre de lieux en 2018:"
+cat $CHEMIN/2018/* | grep Location | wc -l
